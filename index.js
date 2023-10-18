da0eab90ee592c8d383a79e33dfe967ff029eed5
Saurav Raj <saurav98raj@gmail.com>
2023-10-18 17:23:11 +0530

    initial commit2

--- index.js
+++ index.js
@@ -11,6 +11,36 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    let temp = this.head;
+    let pre = this.head;
+    if (!temp) {
+      return 'Empty List';
+    }
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+  }
 }
 
-let myLinkedList = LinkedList(4);
+let myLinkedList = new LinkedList(7);
+myLinkedList.push(4);
+console.log(myLinkedList);
+myLinkedList.pop();
+console.log(myLinkedList);
