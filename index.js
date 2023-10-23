107c99e73740455885cce561e4742f0c5564c87d
Saurav Raj <saurav98raj@gmail.com>
2023-10-23 15:32:03 +0530

    Linked List FinishedPre

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ class LinkedList {
     let temp = this.head;
     let pre = this.head;
     if (!temp) {
-      return 'Empty List';
+      return undefined;
     }
     while (temp.next) {
       pre = temp;
@@ -42,12 +42,91 @@ class LinkedList {
     }
     return temp;
   }
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  shift() {
+    if (!this.head) return 'Empty Node';
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return temp;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) throw Error('Index out of bounds');
+    let current = this.head;
+    for (let i = 0; i < index && current !== null; i++) {
+      current = current.next;
+    }
+    return current ? current : undefined;
+  }
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+    const newNode = new Node(value);
+    const temp = this.get(index - 1);
+    newNode.next = temp.next;
+    temp.next = newNode;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return undefined;
+
+    const before = this.get(index - 1);
+    const temp = before.next;
+    before.next = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let next = temp.next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
+  }
 }
 
 let myLinkedList = new LinkedList(7);
 myLinkedList.push(4);
+myLinkedList.push(8);
 console.log(myLinkedList);
-myLinkedList.pop();
+// myLinkedList.pop();
+myLinkedList.set(1, 77);
 console.log(myLinkedList);
-myLinkedList.pop();
-console.log(myLinkedList);
+// myLinkedList.pop();
+// console.log(myLinkedList);
